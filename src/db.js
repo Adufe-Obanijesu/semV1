4b06076cc871a0a088bdb1e1725d243d329bc23f
Adufe-Obanijesu <obanijesuadufe@gmail.com>
2021-08-20 22:46:54 +0100

    Making subtle changes

--- src/db.js
+++ src/db.js
@@ -1,13 +1,28 @@
 const db = [
 	{
-		name: "Adufe Obanijesu",
-		meterNumber: "234",
-		accessNumber: "08146917054"
+		name: "Mr. Joseph Smart",
+		meterNumber: "0123456789",
+		accessNumber: "08133490959"
 	},
 	{
-		name: "Akinboyede Joseph",
-		meterNumber: "123",
-		accessNumber: "08035553294"
+		name: "Mr. Abiola Smart",
+		meterNumber: "1234567890",
+		accessNumber: "07069602890"
+	},
+	{
+		name: "Mr. Daniel Smart",
+		meterNumber: "2345678901",
+		accessNumber: "08169961707"
+	},
+	{
+		name: "Mr. Dhikrulah Smart",
+		meterNumber: "3456789012",
+		accessNumber: "08130947160"
+	},
+	{
+		name: "Engr. Lawal Smart",
+		meterNumber: "4567890123",
+		accessNumber: "08038490867"
 	}
 ]
 
